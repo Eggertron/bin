bbf130020064a181dc5189134df5e5347bdafd27
Egger Haaaaan <bboyhan@hotmail.com>
2017-10-26 11:46:00 -0400

    Add files via upload

---
+++ fix_touchpad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### These settings adjustments are for Dell XPS 13
+### to make the touchpad more natural.
+
+synclient VertScrollDelta=-27
+synclient HorizScrollDelta=-27
+synclient PalmDetect=1
