c9dda4b40278f7eca04f76fa3fe64eb3c0970159
Egger Haaaaan <bboyhan@hotmail.com>
2021-07-13 22:20:51 -0400

    Update YouTubeToMp3.sh

--- YouTubeToMp3.sh
+++ YouTubeToMp3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ "$1" == "" ]]; then echo "Missing YouTube URL"; exit 1; fi
+
 VLC_LUA_PATH="/usr/lib/x86_64-linux-gnu/vlc/lua/playlist"
 YOUTUBE_LUA_URL="https://raw.githubusercontent.com/videolan/vlc/master/share/lua/playlist/youtube.lua"
 TRANSCODEMP3="#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100}:std{access=file,mux=raw,dst=output.mp3}"
