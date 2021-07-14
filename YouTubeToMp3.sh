07268638639804547dcf594e2b7750ba2303c053
Egger Haaaaan <bboyhan@hotmail.com>
2021-07-13 23:07:44 -0400

    Update YouTubeToMp3.sh

--- YouTubeToMp3.sh
+++ YouTubeToMp3.sh
@@ -8,6 +8,7 @@ TITLE=`curl -L $1 | grep -o -P '(?<=<title>).*(?=</title>)'`
 TRANSCODEMP3="#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100}:std{access=file,mux=raw,dst=output.mp3}"
 PARAMS='-I dummy --no-sout-video --sout-audio --no-sout-rtp-sap --no-sout-standard-sap --ttl=1 --sout-keep --sout '$TRANSCODEMP3' '$1' vlc://quit'
 PARAMSS='--sout "#transcode{acodec=mp3,ab=128,channels=2,samplerate=44100}:std{access=file,mux=raw,dst=output.mp3}" '$1' vlc://quit'
-echo $PARAMS
+echo "Params: $PARAMS"
 vlc $PARAMS
 mv output.mp3 "$TITLE.mp3"
+echo "File saved as $TITLE.mp3"
